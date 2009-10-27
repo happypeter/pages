95792daed4a30b30e04752d1c4b92e590dad2bde
pet <pet@girl.(none)>
2009-10-27 17:31:43 +0800

    clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,6 @@
+#this is the script I run to push my changes to github
+#so that I can get ride of most trash files
+#TODO: better if I can just save diffs for revisions
 rm -rf  */cache
 rm */edit-lock
 git add .
